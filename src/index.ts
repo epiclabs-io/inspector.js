8d6a631f26a93eafe0ea4138fbd8fdbaeb59bdd2
Siro Blanco <siro@epiclabs.io>
2017-11-17 12:20:02 +0100

    inspector.js: support for web worker. Example in index.html.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 import MpegTSDemuxer from './demuxer/ts/mpegts-demuxer';
 import Mp4Demuxer from './demuxer/mp4/mp4-demuxer';
+import IDemuxer from './demuxer/demuxer';
+import WebWorker from './utils/web-worker';
 
-export function createMpegTSDemuxer(data: Uint8Array): MpegTSDemuxer {
+export function createMpegTSDemuxer(): MpegTSDemuxer {
     return new MpegTSDemuxer();
 }
 
-export function createMp4Demuxer(data: Uint8Array): Mp4Demuxer {
+export function createMp4Demuxer(): Mp4Demuxer {
     return new Mp4Demuxer();
 }
+
+export enum InspectorActionType {
+    CREATE_MP4_DEMUX_JOB = 'CREATE_MP4_DEMUX_JOB',
+    CREATE_MP4_DEMUX_JOB_RESPONSE = 'CREATE_MP4_DEMUX_JOB_RESPONSE',
+    CREATE_MPEGTS_DEMUX_JOB = 'CREATE_MPEGTS_DEMUX_JOB',
+    CREATE_MPEGTS_DEMUX_JOB_RESPONSE = 'CREATE_MPEGTS_DEMUX_JOB_RESPONSE',
+    EXECUTE_JOB_APPEND = 'EXECUTE_JOB_APPEND',
+    EXECUTE_JOB_APPEND_RESPONSE = 'EXECUTE_JOB_APPEND_RESPONSE',
+    END_JOB = 'END_JOB',
+    END_JOB_RESPONSE = 'END_JOB_RESPONSE',
+}
+
+export interface InspectorAction {
+    type: InspectorActionType;
+    job: string;
+    data: any;
+}
+
+onmessage = WebWorker.onMessage;
---
+++ src/utils/web-worker.ts
@@ -0,0 +1,84 @@
+import * as index from '../index';
+import IDemuxer from '../demuxer/demuxer';
+import Mp4Demuxer from '../demuxer/mp4/mp4-demuxer';
+import MpegTSDemuxer from '../demuxer/ts/mpegts-demuxer';
+
+export default class WebWorker {
+    public static onMessage = ((event) => {
+        if (event.data) {
+            switch (event.data.type) {
+                case index.InspectorActionType.CREATE_MP4_DEMUX_JOB:
+                    WebWorker.createMp4DemuxJob(event);
+                    break;
+                case index.InspectorActionType.CREATE_MPEGTS_DEMUX_JOB:
+                    WebWorker.createMpegTSDemuxJob(event);
+                    break;
+                case index.InspectorActionType.EXECUTE_JOB_APPEND:
+                    WebWorker.executeJobAppend(event);
+                    break;
+                case index.InspectorActionType.END_JOB:
+                    WebWorker.endJob(event);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+
+    private static jobs: Map<String, IDemuxer> = new Map<String, IDemuxer>();
+
+    private static createMp4DemuxJob(event: index.InspectorAction): void {
+        const guid: String = WebWorker.createGuid();
+        const demuxer: Mp4Demuxer = index.createMp4Demuxer();
+        WebWorker.jobs.set(guid, demuxer);
+        postMessage({
+            type: index.InspectorActionType.CREATE_MP4_DEMUX_JOB_RESPONSE,
+            job: guid,
+            data: demuxer,
+        });
+    }
+
+    private static createMpegTSDemuxJob(event: index.InspectorAction): void {
+        const guid: String = WebWorker.createGuid();
+        const demuxer: MpegTSDemuxer = index.createMpegTSDemuxer();
+        WebWorker.jobs.set(guid, demuxer);
+        postMessage({
+            type: index.InspectorActionType.CREATE_MPEGTS_DEMUX_JOB_RESPONSE,
+            job: guid,
+            data: demuxer,
+        });
+    }
+
+    private static executeJobAppend(event: index.InspectorAction): void {
+        if (WebWorker.jobs.has(event.data.job)) {
+            const demuxer: IDemuxer = WebWorker.jobs.get(event.data.job);
+            demuxer.append(event.data.data);
+            postMessage({
+                type: index.InspectorActionType.EXECUTE_JOB_APPEND_RESPONSE,
+                job: event.data.job,
+                data: demuxer,
+            });
+        }
+    }
+
+    private static endJob(event: index.InspectorAction): void {
+        if (WebWorker.jobs.has(event.data.job)) {
+            const demuxer: IDemuxer = WebWorker.jobs.get(event.data.job);
+            WebWorker.jobs.get(event.data.job).end();
+            WebWorker.jobs.delete(event.data.job);
+            postMessage({
+                type: index.InspectorActionType.END_JOB_RESPONSE,
+                job: event.data.job,
+                data: demuxer,
+            });
+        }
+    }
+
+    private static createGuid(): String {
+        return `${this.s4()}${this.s4()}-${this.s4()}-${this.s4()}-${this.s4()}-${this.s4()}${this.s4()}${this.s4()}`;
+    }
+
+    private static s4(): String {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+}
