9edc140acb51e5bfd1c26af85206539b465f18ad
Stephan Hesse <tchakabam@gmail.com>
2018-05-31 21:01:32 +0200

    frame: add duration prop and comment

--- src/demuxer/frame.ts
+++ src/demuxer/frame.ts
@@ -1,9 +1,16 @@
 export class Frame {
+
+    // fixme: should be an enum
     public static IDR_FRAME: string = 'I';
     public static P_FRAME: string = 'P';
     public static B_FRAME: string = 'B';
 
-    constructor (public frameType: string, public timeUs: number, public size: number) {
+    constructor (
+        public frameType: string,
+        public timeUs: number,
+        public size: number,
+        public duration: number = 0
+    ) {
         // do nothing
     }
 }
