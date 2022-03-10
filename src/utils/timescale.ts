3259b78119f188913492beabea5468d967002985
Stephan Hesse <stephan@emliri.com>
2022-03-10 21:48:38 +0100

    utils: timescale: add MPEG_CLOCK_HZ const

--- src/utils/timescale.ts
+++ src/utils/timescale.ts
@@ -8,7 +8,9 @@ export function toSecondsFromMicros(us) {
     return us / MICROSECOND_TIMESCALE;
 }
 
+export const MPEG_CLOCK_HZ = 90000;
+
 export function mpegClockTimeToMicroSecs(time: number): number {
-    return toMicroseconds(time, 90000);
+    return toMicroseconds(time, MPEG_CLOCK_HZ);
 }
 
