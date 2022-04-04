8f0caadcd11c3fb24a70fa671c6eddf35c1ad029
J. Oliva <jeoliva@gmail.com>
2022-04-04 10:29:07 +0200

    Audit fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,19 @@
 const path = require("path");
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 const PATHS = {
     entryPoint: path.resolve(__dirname, 'src/index.ts'),
     bundles: path.resolve(__dirname, 'lib'),
 };
 
-const config = {
-    // These are the entry point of our library. We tell webpack to use
-    // the name we assign later, when creating the bundle. We also use
-    // the name to filter the second entry point for applying code
-    // minification via UglifyJS
+const mode = process.env.NODE_ENV || "development"
+module.exports = {
+    mode: mode,
     entry: {
         'inspectorjs-lib': [PATHS.entryPoint],
         'inspectorjs-lib.min': [PATHS.entryPoint]
     },
-    // The output defines how and where we want the bundles. The special
-    // value `[name]` in `filename` tell Webpack to use the name we defined above.
-    // We target a UMD and name it MyLib. When including the bundle in the browser
-    // it will be accessible at `window.MyLib`
     output: {
         path: PATHS.bundles,
         filename: '[name].js',
@@ -27,25 +21,15 @@ const config = {
         library: 'inspectorjs',
         umdNamedDefine: true
     },
-    // Add resolve for `tsx` and `ts` files, otherwise Webpack would
-    // only look for common JavaScript file extension (.js)
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+    optimization: {
+        minimize: mode === "production",
+        minimizer: [new TerserPlugin()],
     },
-    // Activate source maps for the bundles in order to preserve the original
-    // source when the user debugs the application
-    // devtool: 'source-map',
-    devtool: 'eval', // https://github.com/webpack/webpack/issues/1487#issuecomment-209299242
+    performance: {
+        maxAssetSize: 300000,
+        maxEntrypointSize: 300000
+    },
     plugins: [
-        // Apply minification only on the second bundle by
-        // using a RegEx on the name, which must end with `.min.js`
-        // NB: Remember to activate sourceMaps in UglifyJsPlugin
-        // since they are disabled by default!
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            sourceMap: true,
-            include: /\.min\.js$/,
-        }),
         new HtmlWebpackPlugin({
             inject: 'head',
             filename: 'samples/index.html',
@@ -57,23 +41,31 @@ const config = {
             template: 'samples/index_webworkers.html'
         })
     ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    devtool: 'eval',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './'),
+        },
+        hot: true,
+    },
     module: {
         // Webpack doesn't understand TypeScript files and a loader is needed.
         // `node_modules` folder is excluded in order to prevent problems with
         // the library dependencies, as well as `__tests__` folders that
         // contain the tests for the library
-        loaders: [{
+        rules: [{
             test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
             exclude: /node_modules/,
-            query: {
-                // we don't want any declaration file in the bundles
-                // folder since it wouldn't be of any use ans the source
-                // map already include everything for debugging
-                declaration: false,
+            use: {
+                loader: 'ts-loader',
+                options: {
+                    // disable type checker - we will use it in fork plugin
+                    transpileOnly: true
+                }
             }
         }]
     }
-};
-
-module.exports = config;
+}
