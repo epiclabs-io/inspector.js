173882b63765d6e4dbfd4a312008d5e9a100edab
Stephan Hesse <stephan@emliri.com>
2022-07-06 00:34:22 +0200

    frame: print values to assertion error messages

--- src/demuxer/frame.ts
+++ src/demuxer/frame.ts
@@ -12,13 +12,13 @@ export class Frame {
         private _bytesOffset: number = NaN
     ) {
         if (dts < 0 || !Number.isSafeInteger(dts)) {
-            throw new Error(`Frame: DTS has to be positive safe-integer value`);
+            throw new Error(`Frame: DTS has to be positive safe-integer value but is ${dts}`);
         }
         if (size < 0 || !Number.isSafeInteger(size)) {
-            throw new Error(`Frame: Size has to be positive safe-integer value`);
+            throw new Error(`Frame: Size has to be positive safe-integer value but is ${size}`);
         }
         if (duration < 0 || !Number.isSafeInteger(duration)) {
-            throw new Error(`Frame: Duration has to be positive safe-integer value`);
+            throw new Error(`Frame: Duration has to be positive safe-integer value but is ${duration}`);
         }
         this.setPresentationTimeOffset(_cto);
     }
@@ -37,14 +37,14 @@ export class Frame {
      */
     setPresentationTimeOffset(cto: number) {
         if (cto < 0 || !Number.isSafeInteger(cto)) {
-            throw new Error(`Frame: CTO has to be positive safe-integer value`);
+            throw new Error(`Frame: CTO has to be positive safe-integer value but is ${cto}`);
         }
         this._cto = cto;
     }
 
     setBytesOffset(bytesOffset: number) {
         if (bytesOffset < 0 || !Number.isSafeInteger(bytesOffset)) {
-            throw new Error(`Frame: Bytes-offset has to be positive safe-integer value`);
+            throw new Error(`Frame: Bytes-offset has to be positive safe-integer value but is ${bytesOffset}`);
         }
         this._bytesOffset = bytesOffset;
     }
