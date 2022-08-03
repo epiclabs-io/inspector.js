49efd93f949fc06070e4c78794fdc71bc806d96d
Stephan Hesse <stephan@emliri.com>
2022-08-03 23:23:51 +0200

    parsePesHeaderTimestamps: compute and return header remainder length (needed to skip forward to data section of payload after parsing ext fields)

--- src/demuxer/ts/payload/pes-header.ts
+++ src/demuxer/ts/payload/pes-header.ts
@@ -1,6 +1,6 @@
 import { BitReader } from "../../../utils/bit-reader";
 
-export function parsePesHeaderTimestamps(packet: BitReader): [number, number] {
+export function parsePesHeaderTimestamps(packet: BitReader): [number, number, number] {
     /**
      * Thanks to Videojs/Muxjs for this bit, which does well the
      * trick around 32-bit unary bit-ops and 33 bit numbers :)
@@ -14,10 +14,18 @@ export function parsePesHeaderTimestamps(packet: BitReader): [number, number] {
     // most significant bits and then multiply by 4 (equal to a left-shift
     // of 2) before we add the final 2 least significant bits of the
     // timestamp (equal to an OR.)
+
+    packet.skipBytes(1); // todo: parse the data-alignment idc
+
     const ptsDtsFlags = packet.readByte();
-    packet.skipBytes(1);
+
+    const pesHeaderRemainderLen = packet.readByte();
+
     let pts = NaN;
     let dts = NaN;
+
+    let packetRemainder = packet.remainingBytes();
+
     if (ptsDtsFlags & 0xC0) {
         // the PTS and DTS are not written out directly. For information
         // on how they are encoded, see
@@ -41,5 +49,8 @@ export function parsePesHeaderTimestamps(packet: BitReader): [number, number] {
             dts += (lastByte & 0x06) >>> 1; // OR by the two LSBs
         }
     }
-    return [dts, pts];
+
+    let bytesRead = packetRemainder - packet.remainingBytes()
+
+    return [dts, pts, pesHeaderRemainderLen - bytesRead];
 }
