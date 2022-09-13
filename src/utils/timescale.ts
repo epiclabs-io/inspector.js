36227fa9a6e53ec3dd0b4bf8da073d67991d2b36
Stephan Hesse <stephan@emliri.com>
2022-09-13 02:13:03 +0200

    timescale: rm toSecondsFromMicros & mpegClockTimeToMicroSecs (not needed anymore);  replace with mpegClockTimeToSecs plain computation

--- src/utils/timescale.ts
+++ src/utils/timescale.ts
@@ -4,13 +4,9 @@ export function toMicroseconds(value, timescale) {
     return MICROSECOND_TIMESCALE * value / timescale;
 }
 
-export function toSecondsFromMicros(us) {
-    return us / MICROSECOND_TIMESCALE;
-}
-
 export const MPEG_CLOCK_HZ = 90000;
 
-export function mpegClockTimeToMicroSecs(time: number): number {
-    return toMicroseconds(time, MPEG_CLOCK_HZ);
+export function mpegClockTimeToSecs(time: number): number {
+    return time / MPEG_CLOCK_HZ;
 }
 
